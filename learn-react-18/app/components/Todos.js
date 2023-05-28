d606d2277493d9e1c35785142934fbb4bb90210b
boboPrem1 <boboprem1@mail.com>
2023-05-28 04:11:07 +0000

    Writting markup with react

---
+++ learn-react-18/app/components/Todos.js
@@ -0,0 +1,15 @@
+export default function Todos() {
+    return (
+        <div className="w-1/4 py-3 px-4 flex flex-row-reverse justify-between bg-white/25 rounded-xl m-3">
+            <div>
+                <h1 className="mt-1 text-white">Juvanio Amouzougan's todos</h1>
+                <ul className="text-xs ml-3 list-none hover:ml-6 hover:list-disc transition-all">
+                    <li>Invent new traffic lights</li>
+                    <li>Rehearse a movie scene</li>
+                    <li>Improve the spectrum technology</li>
+                </ul>
+            </div>
+            <img src="https://i.imgur.com/yXOvdOSs.jpg" alt="Juvanio Amouzougan" className="rounded-xl"/>
+        </div>
+    )
+}
--- learn-react-18/app/page.js
+++ learn-react-18/app/page.js
@@ -1,7 +1,14 @@
 import "./globale.css";
 import Gallery from "@/app/components/Gallery";
-import { Profile } from "@/app/components/Gallery"
+import {Profile} from "@/app/components/Gallery"
+import Todos from "@/app/components/Todos";
 
-export default function Home (){
-  return (<Profile />)
+export default function Home() {
+    return (
+        <>
+            <Todos/>
+            <Todos/>
+            <Todos/>
+        </>
+    )
 }
